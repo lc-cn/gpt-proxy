b6ced360ea8dc307197b1e23cfe21b38a61030b1
凉菜 <1659488338@qq.com>
2024-01-04 16:03:07 +0800

    更新依赖版本

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-import express, { json, urlencoded } from 'express';
-import dotenv from 'dotenv';
+import {SERVER_PORT} from "./config.js";
+
+const Koa = require('koa');
+const KoaBodyParser = require("koa-bodyparser");
+const Router = require("@koa/router");
+const dotenv=require('dotenv')
 dotenv.config()
 import { completions, chatCompletions } from './routes.js';
-import { corsMiddleware, rateLimitMiddleware } from './middlewares.js';
-import { DEBUG, SERVER_PORT } from './config.js';
-
-let app = express();
-
 process.on("uncaughtException", function (err) {
     if (DEBUG) console.error(`Caught exception: ${err}`);
 });
 
-// Middlewares
-app.use(corsMiddleware);
-app.use(rateLimitMiddleware);
-app.use(json());
-app.use(urlencoded({ extended: true }));
-
+const router = new Router();
+const koa = new Koa()
+koa.use(KoaBodyParser()).use(router.routes()).use(router.allowedMethods());
 // Register routes
-app.get("/", async function (req, res) {
-    res.set("Content-Type", "application/json");
-    return res.status(200).send({
+router.get('home',"/", async function (ctx) {
+    ctx.body ={
         status: true,
         name: "Lc-cn Reverse Proxy",
         version: "1.0.0",
-    });
+    }
 });
-app.post("/v1/completions", completions);
-app.post("/v1/chat/completions", chatCompletions);
+router.post("/v1/completions", completions);
+router.post("/v1/chat/completions", chatCompletions);
 
 // Start server
-app.listen(SERVER_PORT, () => {
+koa.listen(SERVER_PORT, () => {
     console.log(`Listening on ${SERVER_PORT} ...`);
 });
--- routes.js
+++ routes.js
@@ -1,28 +1,27 @@
 import { getOpenAIKey } from "./functions.js"
 import {OpenAI} from "openai";
 
-async function completions(req, res) {
-    const authorization=req.headers?.authorization||req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
+async function completions(ctx) {
+    const authorization=ctx.req.headers?.authorization||ctx.req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
     const apiKey=authorization.split(" ")[1];
     const openAi=new OpenAI({
         apiKey,
     })
     try{
-        res.status(200).send(await openAi.completions.create(req.body));
+        ctx.body=await openAi.completions.create(ctx.req.body)
     }catch (e){
-        res.status(e.status||500).send(e);
+        ctx.body=e
     }
 }
 
-async function chatCompletions(req, res) {
-    const authorization=req.headers?.authorization||req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
+async function chatCompletions(ctx, res) {
+    const authorization=ctx.req.headers?.authorization||ctx.req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
     const apiKey=authorization.split(" ")[1];
     const openAi=new OpenAI({apiKey})
-    console.log(req);
     try{
-        res.status(200).send(await openAi.chat.completions.create(req.body));
+        ctx.body=await openAi.chat.completions.create(ctx.request.body)
     }catch (e){
-        res.status(e.status||500).send(e);
+        ctx.body=e
     }
 }
 
