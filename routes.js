36a606667657005e074da0690e8bb91fe4d6b0e6
凉菜 <1659488338@qq.com>
2024-01-04 15:49:34 +0800

    更新依赖版本

--- routes.js
+++ routes.js
@@ -4,15 +4,26 @@ import {OpenAI} from "openai";
 async function completions(req, res) {
     const authorization=req.headers?.authorization||req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
     const apiKey=authorization.split(" ")[1];
-    const openAi=new OpenAI({apiKey})
-    res.status(200).send(await openAi.completions.create(req.body));
+    const openAi=new OpenAI({
+        apiKey,
+    })
+    try{
+        res.status(200).send(await openAi.completions.create(req.body));
+    }catch (e){
+        res.status(e.status||500).send(e);
+    }
 }
 
 async function chatCompletions(req, res) {
     const authorization=req.headers?.authorization||req.headers?.Authorization|| `Bearer ${getOpenAIKey()}`;
     const apiKey=authorization.split(" ")[1];
     const openAi=new OpenAI({apiKey})
-    res.status(200).send(await openAi.chat.completions.create(req.body));
+    console.log(req.body);
+    try{
+        res.status(200).send(await openAi.chat.completions.create(req.body));
+    }catch (e){
+        res.status(e.status||500).send(e);
+    }
 }
 
 export { completions, chatCompletions };
